96efe35faa82571a5741d92675b474d2ef3cd572
Aaron24 <42289595+shinganlin@users.noreply.github.com>
2020-02-22 23:30:06 +0800

    Add files via upload

---
+++ BST.py
@@ -0,0 +1,33 @@
+class Node(object):
+    def __init__(self, key:int):
+        self.left_node = None
+        self.right_node = None
+        self.parent_node = None
+        self.key = key
+
+class BST(object):
+    def __init__(self, root:Node):
+        self.root = root
+    
+    def add_node(self, node:Node):
+        current_node = self.root
+        while True:
+            if node.key == current_node.key:
+                raise Exception('same key value is not allow !')
+            elif node.key < current_node.key:
+                if current_node.left_node:
+                    current_node = current_node.left_node
+                else:
+                    node.parent_node = current_node
+                    current_node.left_node = node
+                    break
+            elif node.key > current_node.key:
+                if current_node.right_node:
+                    current_node = current_node.right_node
+                else:
+                    node.parent_node = current_node
+                    current_node.right_node = node
+                    break
+
+
+            
